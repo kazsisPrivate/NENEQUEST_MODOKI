77ec0db3182ea4737d4cfcdea4a40a29727956a6
kazsisPrivate <kazsis0703.git.private@gmail.com>
2022-02-06 11:14:38 +0900

    Add explanations of codes

--- NENEQUEST_MODOKI/src/CharaGraphics.cpp
+++ NENEQUEST_MODOKI/src/CharaGraphics.cpp
@@ -7,120 +7,87 @@ int CharaGraphics::mEne1to2Handle[2][4];
 int CharaGraphics::mEne3to7Handle[5][8];
 int CharaGraphics::mEneBossHandle[24];
 
-//CharaGraphics::CharaGraphics() { //‰æ‘œ‚Ì“Ç‚İ‚İ‚ğs‚¤
-//	LoadDivGraph("images/player_1-1-3.png", 24, 2, 12, 500, 283, mPl1to3Handle[0]); 
-//	LoadDivGraph("images/player_2-1-1.png", 24, 2, 12, 500, 283, mPl1to3Handle[1]);
-//	LoadDivGraph("images/player_3-1-1.png", 24, 2, 12, 500, 283, mPl1to3Handle[2]);
-//	LoadDivGraph("images/player_4-1-3.png", 32, 2, 16, 500, 283, mPl4Handle);
-//
-//	LoadDivGraph("images/enemy_1.png", 4, 2, 2, 250, 177, mEne1to2Handle[0]);
-//	LoadDivGraph("images/enemy_2.png", 4, 2, 2, 250, 177, mEne1to2Handle[1]);
-//	LoadDivGraph("images/enemy_3.png", 8, 2, 4, 393, 177, mEne3to7Handle[0]);
-//	LoadDivGraph("images/enemy_4-3.png", 8, 2, 4, 225, 192, mEne3to7Handle[1]);
-//	LoadDivGraph("images/enemy_5-3.png", 8, 2, 4, 225, 192, mEne3to7Handle[2]);
-//	LoadDivGraph("images/enemy_6.png", 8, 2, 4, 225, 190, mEne3to7Handle[3]);
-//	LoadDivGraph("images/enemy_7-3.png", 8, 2, 4, 303, 151, mEne3to7Handle[4]);
-//	LoadDivGraph("images/enemy_5-5.png", 24, 2, 12, 958, 600, mEneBossHandle);
-//}
 
+void CharaGraphics::Initialize() {	// ‰æ‘œ‚Ì“Ç‚İ‚İ
+	// Player1`3‚Ì‰æ‘œ‚Ì“Ç‚İ‚İ
+	LoadDivGraph("images/player1.png", 24, 2, 12, 500, 283, mPl1to3Handle[0]);
+	LoadDivGraph("images/player2.png", 24, 2, 12, 500, 283, mPl1to3Handle[1]);
+	LoadDivGraph("images/player3.png", 24, 2, 12, 500, 283, mPl1to3Handle[2]);
 
-//CharaGraphics::~CharaGraphics() {
-//	for (int i = 0; i < 3; i++) {
-//		for (int j = 0; j < 24; j++) {
-//			DeleteGraph(mPl1to3Handle[i][j]);
-//		}
-//	}
-//
-//	for (int i = 0; i < 32; i++) {
-//		DeleteGraph(mPl4Handle[i]);
-//	}
-//
-//	for (int i = 0; i < 2; i++) {
-//		for (int j = 0; j < 4; j++) {
-//			DeleteGraph(mEne1to2Handle[i][j]);
-//		}
-//	}
-//
-//	for (int i = 0; i < 5; i++) {
-//		for (int j = 0; j < 8; j++) {
-//			DeleteGraph(mEne3to7Handle[i][j]);
-//		}
-//	}
-//
-//	for (int i = 0; i < 24; i++) {
-//		DeleteGraph(mEneBossHandle[i]);
-//	}
-//}
+	// Player4‚Ì‰æ‘œ‚Ì“Ç‚İ‚İ
+	LoadDivGraph("images/player4.png", 32, 2, 16, 500, 283, mPl4Handle);
 
+	// Enemy1`2‚Ì‰æ‘œ‚Ì“Ç‚İ‚İ
+	LoadDivGraph("images/enemy1.png", 4, 2, 2, 250, 177, mEne1to2Handle[0]);
+	LoadDivGraph("images/enemy2.png", 4, 2, 2, 250, 177, mEne1to2Handle[1]);
 
-void CharaGraphics::Initialize() {	//‰æ‘œ‚Ì“Ç‚İ‚İ
-	LoadDivGraph("images/player_1-1-3.png", 24, 2, 12, 500, 283, mPl1to3Handle[0]);
-	LoadDivGraph("images/player_2-1-1.png", 24, 2, 12, 500, 283, mPl1to3Handle[1]);
-	LoadDivGraph("images/player_3-1-1.png", 24, 2, 12, 500, 283, mPl1to3Handle[2]);
-	LoadDivGraph("images/player_4-1-3.png", 32, 2, 16, 500, 283, mPl4Handle);
+	// Enemy3`7‚Ì‰æ‘œ‚Ì“Ç‚İ‚İ
+	LoadDivGraph("images/enemy3.png", 8, 2, 4, 393, 177, mEne3to7Handle[0]);
+	LoadDivGraph("images/enemy4.png", 8, 2, 4, 225, 192, mEne3to7Handle[1]);
+	LoadDivGraph("images/enemy5.png", 8, 2, 4, 225, 192, mEne3to7Handle[2]);
+	LoadDivGraph("images/enemy6.png", 8, 2, 4, 225, 190, mEne3to7Handle[3]);
+	LoadDivGraph("images/enemy7.png", 8, 2, 4, 303, 151, mEne3to7Handle[4]);
 
-	LoadDivGraph("images/enemy_1.png", 4, 2, 2, 250, 177, mEne1to2Handle[0]);
-	LoadDivGraph("images/enemy_2.png", 4, 2, 2, 250, 177, mEne1to2Handle[1]);
-	LoadDivGraph("images/enemy_3.png", 8, 2, 4, 393, 177, mEne3to7Handle[0]);
-	LoadDivGraph("images/enemy_4-3.png", 8, 2, 4, 225, 192, mEne3to7Handle[1]);
-	LoadDivGraph("images/enemy_5-3.png", 8, 2, 4, 225, 192, mEne3to7Handle[2]);
-	LoadDivGraph("images/enemy_6.png", 8, 2, 4, 225, 190, mEne3to7Handle[3]);
-	LoadDivGraph("images/enemy_7-3.png", 8, 2, 4, 303, 151, mEne3to7Handle[4]);
-	LoadDivGraph("images/enemy_5-5.png", 24, 2, 12, 958, 600, mEneBossHandle);
+	// EnemyBoss‚Ì‰æ‘œ‚Ì“Ç‚İ‚İ
+	LoadDivGraph("images/enemyboss.png", 24, 2, 12, 958, 600, mEneBossHandle);
 }
 
 
 void CharaGraphics::Finalize() {	// ‰æ‘œ‚Ìíœ
+	// Player1`3‚Ì‰æ‘œ‚Ìíœ
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 24; j++) {
 			DeleteGraph(mPl1to3Handle[i][j]);
 		}
 	}
 
+	// Player4‚Ì‰æ‘œ‚Ìíœ
 	for (int i = 0; i < 32; i++) {
 		DeleteGraph(mPl4Handle[i]);
 	}
 
+	// Enemy1`2‚Ì‰æ‘œ‚Ìíœ
 	for (int i = 0; i < 2; i++) {
 		for (int j = 0; j < 4; j++) {
 			DeleteGraph(mEne1to2Handle[i][j]);
 		}
 	}
 
+	// Enemy3`7‚Ì‰æ‘œ‚Ìíœ
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 8; j++) {
 			DeleteGraph(mEne3to7Handle[i][j]);
 		}
 	}
 
+	// EnemyBoss‚Ì‰æ‘œ‚Ìíœ
 	for (int i = 0; i < 24; i++) {
 		DeleteGraph(mEneBossHandle[i]);
 	}
 }
 
 
-const int* CharaGraphics::GetGraHandle(const int type, const int num) {
-
-	if (type == 0) {
-		if (num != 4) {
-			return mPl1to3Handle[num-1];
+const int* CharaGraphics::GetGraHandle(const int type, const int charaId) {
+	if (type == 0) {	// Player
+		if (charaId != 4) {
+			return mPl1to3Handle[charaId-1];
 		}
 		else {
 			return mPl4Handle;
 		}
 	}
-	else {
-		if (num < 3) {
-			return mEne1to2Handle[num-1];
+	else {	// Enemy
+		if (charaId <= 2) {
+			return mEne1to2Handle[charaId-1];
 		}
-		else if (num < 8) {
-			return mEne3to7Handle[num-3];
+		else if (charaId <= 7) {
+			return mEne3to7Handle[charaId-3];
 		}
-		else if (num == 8){
+		else if (charaId == 8){
 			return mEneBossHandle;
 		}
 		else {
-			return 0;
+			return NULL;
 		}
 	}
 }
