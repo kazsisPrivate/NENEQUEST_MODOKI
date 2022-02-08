3ff22f4b8d8a5328ffc4219f09dd5b0eb3bb1452
kazsisPrivate <kazsis0703.git.private@gmail.com>
2022-02-08 14:55:44 +0900

    Create Item1

---
+++ NENEQUEST_MODOKI/src/Item1.cpp
@@ -0,0 +1,25 @@
+// 赤りんご（回復系アイテム，回復量2）
+#include "Item1.h"
+#include "DxLib.h"
+#include "ItemGraphics.h"
+
+
+Item1::Item1(ItemChanger* changer, const int itemIdx, const int x, const int y)
+	: Item(changer, ItemGraphics::GetGraHandle(1), itemIdx, x, y) {
+}
+
+
+void Item1::Initialize() {
+	// 当たり判定関連の設定
+	mImgW = 100, mImgH = 90;
+	mHitRangeW = 23, mHitRangeH = 15;
+	mPlIsHit = false;
+	mPlAIsHit = false;
+
+	// その他の設定
+	mSpeed = 2.0f;
+	mHealPower = 2;	// 回復効果あり
+	mSpeedPower = 1.0f;
+	mAttackPower = 1;
+	mItemId = 1;
+}
---
+++ NENEQUEST_MODOKI/src/Item1.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// 赤りんご（回復系アイテム，回復量2）
+
+#include "Item.h"
+
+class Item1 : public Item {
+public:
+	Item1(ItemChanger* changer, const int itemIdx, const int x, const int y);
+	void Initialize() override;
+};
