5ef458094e46b421db4ad7d41c8aae9831730711
kazsisPrivate <kazsis0703.git.private@gmail.com>
2022-02-08 14:57:47 +0900

    Add GetInstance method

--- NENEQUEST_MODOKI/src/SceneMgr.cpp
+++ NENEQUEST_MODOKI/src/SceneMgr.cpp
@@ -6,20 +6,34 @@
 #include "SceneMgr.h"
 
 
-SceneMgr::SceneMgr() :
-	mSceneNext(eSceneNone) //次のシーン管理変数
-{
-	mScene = (BaseScene*) new MenuScene(this);
+SceneMgr* SceneMgr::mSceneMgr;
+
+
+SceneMgr::SceneMgr() : mSceneNext(eSceneNone) {
+}
+
+
+SceneMgr* SceneMgr::GetInstance() {
+	if (!SceneMgr::mSceneMgr) {
+		SceneMgr::mSceneMgr = new SceneMgr();
+		SceneMgr::mSceneMgr->Initialize();
+	}
+
+	return SceneMgr::mSceneMgr;
 }
 
 
 void SceneMgr::Initialize() {
-	mScene->Initialize();
+	mScene = (BaseScene*)MenuScene::GetInstance();
+	//mScene->Initialize();
 }
 
 
 void SceneMgr::Finalize() {
 	mScene->Finalize();
+
+	delete mSceneMgr;
+	mSceneMgr = NULL;
 }
 
 
@@ -27,24 +41,24 @@ void SceneMgr::Update() {
 	if (mSceneNext != eSceneNone) { // 次のシーンがセットされているとき
 		// 現在のシーンの終了処理を実行
 		mScene->Finalize(); 
-		delete mScene;
+		//delete mScene;
 
 		switch (mSceneNext) { // mScenNextで指定されたシーンに変更
 			case eSceneMenu:
-				mScene = (BaseScene*) new MenuScene(this);
+				mScene = (BaseScene*) MenuScene::GetInstance();
 				break;
 			case eSceneGame:
-				mScene = (BaseScene*) new GameScene(this);
+				mScene = (BaseScene*) GameScene::GetInstance();
 				break;
 			case eSceneConfig:
-				mScene = (BaseScene*) new ConfigScene(this);
+				mScene = (BaseScene*) ConfigScene::GetInstance();
 				break;
 		}
 		// 次のシーン情報を空にする
 		mSceneNext = eSceneNone;    
 
 		// シーンを初期化
-		mScene->Initialize();    
+		//mScene->Initialize();    
 	}
 
 	// シーンの更新
--- NENEQUEST_MODOKI/src/SceneMgr.h
+++ NENEQUEST_MODOKI/src/SceneMgr.h
@@ -4,18 +4,26 @@
 #include "BasicFunc.h"
 #include "BaseScene.h"
 
+
+class MenuScene;
+class GameScene;
+class ConfigScene;
+
+
 class SceneMgr : public SceneChanger, BasicFunc {
-
-private:
-	BaseScene* mScene;    //シーン管理変数
-	EScene mSceneNext;    //次のシーン管理変数
-
 public:
 	SceneMgr();
+	static SceneMgr* GetInstance();
 	void Initialize() override;
 	void Finalize() override;
 	void Update() override;
 	void Draw() override;
 	void ChangeScene(const EScene sceneNext) override;	// 引数SceneNextにシーンを変更する
 
+private:
+	BaseScene* mScene;    //シーン管理変数
+	EScene mSceneNext;    //次のシーン管理変数
+
+private:
+	static SceneMgr* mSceneMgr;
 };
