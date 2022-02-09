1acd23ff0ce87ae40dde2206dc248b1e0cdc6724
kazsisPrivate <kazsis0703.git.private@gmail.com>
2022-02-09 10:34:04 +0900

    Create PlayerHpGauge

---
+++ NENEQUEST_MODOKI/src/PlayerHpGauge.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "BasicFunc.h"
+
+
+class PlayerHpGauge : public BasicFunc {
+public:
+	void Initialize() override;
+	void Finalize() override;
+	void Update() override;
+	void Draw() override;
+	void SetPlayerHp(const int plHp);
+
+private:
+	int mPlHp;
+	int mPlFaceHandle;
+	int mGaugeColor;
+};
