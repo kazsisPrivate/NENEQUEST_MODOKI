4ad2427bcd5b167b9881a5b169b30b3c4cc75b51
kazsisPrivate <kazsis0703.git.private@gmail.com>
2022-02-09 10:40:57 +0900

    Create Items of item boxes

---
+++ NENEQUEST_MODOKI/src/Item13.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// 樽2（箱，Item1～7がランダムで出てくる）
+
+#include "ItemBox.h"
+
+
+class Item13 : public ItemBox {
+public:
+	Item13(ItemChanger* changer, const int itemIdx, const int x, const int y);
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+
+private:
+	void CreateBoxItem() override;	// 箱の中から出すitemを決め，作成する
+};
---
+++ NENEQUEST_MODOKI/src/Item14.cpp
@@ -0,0 +1,64 @@
+// 箱（箱，Item8～11（武器）がランダムで出てくる）
+#include "Item14.h"
+#include "DxLib.h"
+#include "ItemGraphics.h"
+#include <time.h>
+
+
+Item14::Item14(ItemChanger* changer, const int itemIdx, const int x, const int y)
+	: ItemBox(changer, ItemGraphics::GetGraHandle(14), itemIdx, x, y) {
+}
+
+
+void Item14::Initialize() {
+	ItemBox::Initialize();
+
+	// 当たり判定関連の設定
+	mImgW = 164, mImgH = 160;
+	mHitRangeW = 35, mHitRangeH = 35;
+
+	// その他の設定
+	mItemId = 13;
+}
+
+
+void Item14::Draw() {
+	ItemBox::Draw();
+}
+
+
+void Item14::Update() {
+	ItemBox::Update();
+}
+
+
+void Item14::CreateBoxItem() {
+	// 1～100のランダムな数値を生成
+	srand((unsigned int)time(NULL));
+	int randNum = rand() % 100 + 1;
+
+	if (randNum <= 25) {	// 25%の確率で
+		// 剣1（Item8）
+		mBoxItemEnum = eItem8;
+		mBoxItemHandle = ItemGraphics::GetGraHandle(8);
+		mBoxIteImgW = 121, mBoxIteImgH = 205;
+	}
+	else if (randNum <= 50) {	// 25%の確率で
+		// 剣2（Item9）
+		mBoxItemEnum = eItem9;
+		mBoxItemHandle = ItemGraphics::GetGraHandle(9);
+		mBoxIteImgW = 153, mBoxIteImgH = 200;
+	}
+	else if (randNum <= 75) {	// 25%の確率で
+		// 斧（Item10）
+		mBoxItemEnum = eItem10;
+		mBoxItemHandle = ItemGraphics::GetGraHandle(10);
+		mBoxIteImgW = 144, mBoxIteImgH = 200;
+	}
+	else {	// 25%の確率で
+		// 弓（Item11）
+		mBoxItemEnum = eItem11;
+		mBoxItemHandle = ItemGraphics::GetGraHandle(11);
+		mBoxIteImgW = 130, mBoxIteImgH = 147;
+	}
+}
